031d53bfde88e9555a6023f6e00237201449769e
NAME <mail@gmail.com>
2023-10-29 10:45:47 -0500

    CH11: Added Spring Clouth Sleuth, Logstash and Zipkin.

--- src/main/java/com/smia/gatewayserver/filters/ResponseFilter.java
+++ src/main/java/com/smia/gatewayserver/filters/ResponseFilter.java
@@ -1,5 +1,7 @@
 package com.smia.gatewayserver.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
+import brave.Span;
+import brave.Tracer;
+import brave.propagation.TraceContext;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -18,18 +23,30 @@ public class ResponseFilter {
     @Autowired
     FilterUtils filterUtils;
 
+    @Autowired
+    Tracer tracer;
+
     @Bean
     public GlobalFilter postGlobalFilter() {
         return (exchange, chain) -> {
+
+            final String traceId = Optional.ofNullable(tracer.currentSpan())
+                    .map(Span::context)
+                    .map(TraceContext::traceIdString)
+                    .orElse("null");
+
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = filterUtils.getCorrelationId(requestHeaders);
+                //String correlationId = filterUtils.getCorrelationId(requestHeaders);
                 String authToken = filterUtils.getAuthToken(requestHeaders);
-                logger.debug("DEBUG.Adding the correlation id to the outbound headers. {}", correlationId);
-                logger.info("INFO.Adding the correlation id to the outbound headers. {}", correlationId);
-                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+                //exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
                 logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
                 exchange.getResponse().getHeaders().add(FilterUtils.AUTH_TOKEN, authToken);
+
+                logger.info(">>> Adding the correlation id to the outbound headers. {} <<<", traceId);
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
+                logger.debug("[Completing outgoing request for {}.]", exchange.getRequest().getURI());
+
             }));
         };
     }
